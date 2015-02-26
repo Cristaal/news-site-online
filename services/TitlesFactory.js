aa19bd245c9dbfbd76adb7916add256039f64ff6
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-25 22:51:14 -0800

    add comments factory amd controllers

--- services/TitlesFactory.js
+++ services/TitlesFactory.js
@@ -2,7 +2,6 @@ newsSite.factory('TitlesFactory', function TitlesFactory() {
   var factory = {};
   factory.titles = [];
   factory.addTitle = function() {
-    debugger;
     factory.titles.push({ title: factory.storyTitle, id: factory.titles.length + 1});
     factory.storyTitle = null;
   };
