92d43340df09c7bb52a4b4d05b82cbc531f7c3ed
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-26 21:23:28 -0800

    add ability to post comment-like entries

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,7 +1,9 @@
 newsSite.controller('CommentsCtrl', function CommentsCtrl($scope, $stateParams, $http, TitlesFactory, UtilitiesFactory) {
   $scope.title = UtilitiesFactory.findById(TitlesFactory.titles, $stateParams.titleId)
   $scope.addComment = function() {
-    $scope.title.comments.push({ comment: $scope.enteredComment });
-    $scope.enteredComment = null;
+    $scope.title.comments = [];
+    $scope.title.comments.push({ comment: $scope.title.comment });
+    debugger;
+    $scope.comment = null;
   }
 });
